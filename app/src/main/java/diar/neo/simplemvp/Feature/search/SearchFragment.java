4086abb6e528be9ab97106b2053cab4880abcf59
neo <shahdad.h@live.com>
2019-07-04 17:44:32 +0430

    compelete search xml file

--- app/src/main/java/diar/neo/simplemvp/Feature/search/SearchFragment.java
+++ app/src/main/java/diar/neo/simplemvp/Feature/search/SearchFragment.java
@@ -1,6 +1,11 @@
 package diar.neo.simplemvp.feature.search;
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import diar.neo.simplemvp.R;
 import diar.neo.simplemvp.base.BaseFragment;
@@ -9,6 +14,26 @@ public class SearchFragment extends BaseFragment implements SearchContract.View 
     @Override
     public void setupViews() {
 
+        EditText edtSearch = rootView.findViewById(R.id.edt_search);
+        edtSearch.requestFocus();
+        showKeyboard();
+
+        edtSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Toast.makeText(getViewContext(), s, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     @Override
@@ -19,5 +44,16 @@ public class SearchFragment extends BaseFragment implements SearchContract.View 
     @Override
     public Context getViewContext() {
         return getContext();
+    }
+
+
+    public void showKeyboard() {
+        InputMethodManager inputMethodManager = (InputMethodManager) getViewContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+    }
+
+    public void closeKeyboard() {
+        InputMethodManager inputMethodManager = (InputMethodManager) getViewContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
     }
 }
